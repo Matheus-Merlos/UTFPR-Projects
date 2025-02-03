272165cf3e3a912c0db1527d185b4f437725eed8
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-02-03 20:15:54 -0300

    fixed error in script

--- run-LRU.sh
+++ run-LRU.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 gcc -o ./out/lru_simulator least-recently-used-graph.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./out/lru_simulator
-clear
+
+if [ $? -eq 0 ]; then
+    echo "Compilação bem-sucedida!"
+    ./out/lru_simulator
+    clear
+else
+    echo "Erro durante a compilação."
+fi
