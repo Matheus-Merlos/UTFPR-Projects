2c21dcbb2ee3a371979c1e18113f59de268650f4
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-02-03 20:06:49 -0300

    Fixed name error

--- run-LRU.sh
+++ run-LRU.sh
@@ -1,3 +1,3 @@
-gcc -o ./out/lru_simulator least-recently-used.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o ./out/lru_simulator least-recently-used-graph.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 ./out/lru_simulator
 clear
